fd48640b400e70942ed65489c2552c36498cfcab
talvaltzer <talvaltzer@10.100.102.5>
2024-11-22 20:29:42 +0700

    update2

--- Maman12/src/Main.java
+++ Maman12/src/Main.java
@@ -7,11 +7,13 @@ public class Main {
 		    // Adding animals
 			animals.add(new Dog("nuggets",1.1,"Blue merle"));
 			animals.add(new Dog("buddy", 2.5, "Brown"));
+			animals.add(new Cat("buddy", 2.5, "Brown"));
 			 for (Animal animal : animals) {
 		            System.out.println(animal);
 		            animal.eat();
 		            animal.sleep();
 		        }
+			 System.out.println(animals.get(1).equals(animals.get(2)));
 			
 	 
 	 }
